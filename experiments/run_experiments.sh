de71faa11424fc6ecd8a1eef3ad1330220b79a3f
Adam <adams.evloeff@yandex.ru>
2026-06-12 11:35:01 +0300

    ci: add GitHub  workflows

---
+++ experiments/run_experiments.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Запуск экспериментов"
+
+# Переход в нужную папку
+cd /mnt/c/Users/Adam/CLionProjects/huffman-arhive/experiments
+
+# Компиляция
+gcc -o experiment experiment.c ../src/archive/compressor.c ../src/tree/huffman_tree.c ../src/queue/priority_queue.c ../src/bits/bit_io.c -I..
+
+# Запуск
+./experiment
+
+echo "Готово!"
