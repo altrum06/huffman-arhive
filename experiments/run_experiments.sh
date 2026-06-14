669aa6c2657e0efa0a8efee1d3357e226440d49f
Adam <adams.evloeff@yandex.ru>
2026-06-14 22:19:56 +0300

    fix

--- experiments/run_experiments.sh
+++ experiments/run_experiments.sh
@@ -6,7 +6,7 @@ echo "Запуск экспериментов"
 cd /mnt/c/Users/Adam/CLionProjects/huffman-arhive/experiments
 
 # Компиляция
-gcc -o experiment experiment.c ../src/archive/compressor.c ../src/tree/huffman_tree.c ../src/queue/priority_queue.c ../src/bits/bit_io.c -I..
+gcc -o experiment experiment.c ../src/archive/compressor.c ../src/tree/huffman_tree.c ../src/queue/priority_queue.c ../src/bits/bit_io.c -I.. -lm
 
 # Запуск
 ./experiment
